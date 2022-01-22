cc8595804b5a586729bebdd70249cca7d30af878
Iagofernandez <43774731+Iagofernandez@users.noreply.github.com>
2022-01-22 09:05:48 +0100

    Añadir nuevo usuario al registro finalizado, inserccion en su tabla correspodiente

--- auxiliar.php
+++ auxiliar.php
@@ -1,7 +1,11 @@
 <?php
 // Variables recogidas desde los input de index.html
 $usuario = $_REQUEST['usuario'];
-$contrasena = $_REQUEST['contrasena'];
+$contrasena = $_REQUEST['contrasinal'];
+$nome = $_REQUEST['nome'];
+$direccion = $_REQUEST['direccion'];
+$telefono = $_REQUEST['telefono'];
+$dni = $_REQUEST['dni'];
 // Variables para realizar la conexión a nuestra bas de datos
 $servername = "localhost";
 $database = "catalogo";
@@ -14,12 +18,12 @@ $conn = mysqli_connect($servername, $usuario_base_datos, $contraseña_base_datos
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-echo "Connected successfully";
+echo "Connected successfully ";
 
 // En la variable $insert_query se ha creado la accion de inserccion en la tabla usuarios
 //Se empelan las variables recibidas desde el html y se insertan como VALUES de mi insercción
-$insert_query = "INSERT INTO usuario(usuario, contrasinal, nome, direccion, telefono, nifdni, tipo_usuario)
-VALUES ('".$usuario."','".$contrasena."','iago','Vigo',12345,'12345678A','a')";
+$insert_query = "INSERT INTO novo_rexistro(usuario, contrasinal, nome, direccion, telefono, nifdni)
+VALUES ('".$usuario."','".$contrasena."','".$nome."','".$direccion."',".$telefono.",'".$dni."')";
 
 // En caso de que la inserccion saliese correctamente, salta este mensaje por pantalla
 If (mysqli_query($conn, $insert_query)) {
