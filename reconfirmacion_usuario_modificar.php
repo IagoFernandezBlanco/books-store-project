38526776a79336403520363a1ec0a235bbde3ab1
Iagofernandez <43774731+Iagofernandez@users.noreply.github.com>
2022-01-25 17:49:54 +0100

    Creada la funcionalidad de modificacion de datos de usuario, salvo el usuario

--- reconfirmacion_usuario_modificar.php
+++ reconfirmacion_usuario_modificar.php
@@ -3,6 +3,20 @@
 // Y hace un update de los campos que han sido modificados por el usuario
 // Con el fin de que pueda modificar los campos que el quiera, salvo su usuario
 
+// Inicio de la session de logue para recoger el nombre de nuestro usuario actual
+// A fin de que puedas hacer solo un update de ese usuario específico
+// nevitando de otras maneras que un usuario pueda modificar otro usuario
+// desde su porpia cuenta
+session_start();
+$usuario = $_SESSION['usuario'];
+
+$contrasena = $_REQUEST['contrasinal'];
+$nome = $_REQUEST['nome'];
+$direccion = $_REQUEST['direccion'];
+$telefono = $_REQUEST['telefono'];
+$nifdni = $_REQUEST['nifdni'];
+
+// echo "Modificando del usuario " .$usuario ."" .$contrasena ."" .$direccion ."" .$telefono ."" .$nifdni;
 // Variables para realizar la conexión a nuestra bas de datos
 $servername = "localhost";
 $database = "catalogo";
@@ -14,6 +28,13 @@ $conn = mysqli_connect($servername, $usuario_base_datos, $contraseña_base_datos
 // Verificamos si la conexion se realiza(las variables que le pasamos como argumento)
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
+}
+
+$query_update_usuario = "UPDATE usuario set contrasinal = '".$contrasena."', nome = '".$nome."', direccion ='".$direccion."', telefono ='".$telefono."', nifdni ='".$nifdni."' where usuario = '".$usuario."'"; 
+
+if(mysqli_query($conn, $query_update_usuario)){
+    echo "usuario modified";
+    header("Location: libros.php");
 }
 // Es necesario cerrar la conexion con nuestra base de datos.
 mysqli_close($conn);
